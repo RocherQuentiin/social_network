b0e131c7a434ef4d0ea51a297cd8e22c3056ff28
Quentin Rocher <qrocher@weyield.io>
2025-12-22 23:04:47 +0100

    possibility to register a user and display it in the list

--- socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/service/UserService.java
+++ socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/service/UserService.java
@@ -1,5 +1,8 @@
 package com.socialnetwork.socialnetwork.business.service;
 
+import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.socialnetwork.socialnetwork.business.interfaces.repository.IUserRepository;
@@ -9,11 +12,13 @@ import com.socialnetwork.socialnetwork.entity.User;
 @Service
 public class UserService implements IUserService{
 	private final IUserRepository repository;
-	
-	public UserService(IUserRepository repository) {
+	private final PasswordEncoder passwordEncoder;
+    
+	public UserService(IUserRepository repository, PasswordEncoder passwordEncoder) {
 		this.repository = repository;
+		this.passwordEncoder = passwordEncoder;
 	}
-	
+    
 	@Override
 	public String getName() {
 		return repository.findAll()
@@ -22,5 +27,18 @@ public class UserService implements IUserService{
 				.map(User::getUsername)
 				.orElse("");
 	}
-	
+
+	@Override
+	public User create(User user) {
+		if (user.getPasswordHash() != null && !user.getPasswordHash().isEmpty()) {
+			user.setPasswordHash(passwordEncoder.encode(user.getPasswordHash()));
+		}
+		return repository.save(user);
+	}
+
+	@Override
+	public List<User> findAllUsers() {
+		return repository.findAll();
+	}
+    
 }
