6afe628bbc49568fdd96364d3587512f9d260d36
fathi marie <fathi.marie1@gmail.com>
2025-12-24 18:11:47 +0100

    page d'accueil v1

---
+++ socialnetwork/src/main/java/com/socialnetwork/socialnetwork/controller/HomeController.java
@@ -0,0 +1,13 @@
+package fr.isep.connect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String afficherAccueil() {
+        return "accueil";
+    }
+}
