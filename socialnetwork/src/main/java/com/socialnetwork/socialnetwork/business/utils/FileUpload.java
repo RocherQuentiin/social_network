0e03ca3f73a46d23acb2bcf4fab871e178c64f6c
akash47 <65364908+Akashos23@users.noreply.github.com>
2026-01-07 14:49:08 +0100

    path prod test

--- socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/utils/FileUpload.java
+++ socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/utils/FileUpload.java
@@ -10,7 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUpload {
-	private static final Path UPLOAD_DIR = Paths.get("src/main/resources/static/upload");
+	private static final Path UPLOAD_DIR = Paths.get("main/resources/static/upload");
 	
 	public static String UploadFile(MultipartFile file) {
 		try {
