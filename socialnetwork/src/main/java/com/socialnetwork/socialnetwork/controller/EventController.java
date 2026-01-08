fd9e3ea162ac3440422e24bad946409f9c9f6485
akash47 <65364908+Akashos23@users.noreply.github.com>
2026-01-08 08:30:35 +0100

    correction supérieur + import inutile

--- socialnetwork/src/main/java/com/socialnetwork/socialnetwork/controller/EventController.java
+++ socialnetwork/src/main/java/com/socialnetwork/socialnetwork/controller/EventController.java
@@ -5,19 +5,15 @@ import java.time.ZonedDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.socialnetwork.socialnetwork.business.interfaces.service.IEventService;
 import com.socialnetwork.socialnetwork.business.interfaces.service.IUserService;
 import com.socialnetwork.socialnetwork.business.utils.Utils;
-import com.socialnetwork.socialnetwork.entity.Connection;
 import com.socialnetwork.socialnetwork.entity.Event;
 import com.socialnetwork.socialnetwork.entity.User;
 
@@ -54,7 +50,7 @@ public String createEvent(HttpServletRequest request, Model model, Event event) 
     ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Europe/Paris"));
     
     if(event.getEventDate().isBefore(now.toLocalDateTime()) || event.getEventDate().isEqual(now.toLocalDateTime())) {
-    	model.addAttribute("errorEvent", "La date doit être spérieur a celle d'aujourd'hui pour la création de l'événement");
+    	model.addAttribute("errorEvent", "La date doit être supérieur a celle d'aujourd'hui pour la création de l'événement");
 		model.addAttribute("event", event);
 		return this.UserController.showUserProfil(request, model);
     }
