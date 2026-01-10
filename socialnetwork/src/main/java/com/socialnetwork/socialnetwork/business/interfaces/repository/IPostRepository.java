109b0cb23f569cfa52ab7d5a2bb929e06bc4f7ad
akash47 <65364908+Akashos23@users.noreply.github.com>
2026-01-10 14:58:15 +0100

    petit correct

--- socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/interfaces/repository/IPostRepository.java
+++ socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/interfaces/repository/IPostRepository.java
@@ -10,35 +10,33 @@ import org.springframework.data.repository.query.Param;
 import com.socialnetwork.socialnetwork.entity.Post;
 
 public interface IPostRepository extends JpaRepository<Post, UUID> {
-	@Query(
-			  value = """
-			    SELECT p.*
-			    FROM post p
-			    LEFT JOIN connection c
-			      ON (c.requester_id = p.author_id OR c.receiver_id = p.author_id)
-			    WHERE
-			      p.visibility_type = 'PUBLIC'
-			      OR (p.visibility_type = 'PRIVATE' AND p.author_id = :userID)
-			      OR (
-			        p.visibility_type = 'FRIENDS'
-			        AND c.connection_status = 'Accepted'
-			        AND (c.requester_id = :userID OR c.receiver_id = :userID)
-			      )
-			      OR (p.visibility_type = 'FRIENDS' AND p.author_id = :userID)
-			    """,
-			  nativeQuery = true
-			)
+	@Query(value = """
+			  SELECT p.* FROM post p
+			  WHERE p.visibility_type = 'PUBLIC'
+			  OR (p.visibility_type = 'PRIVATE'
+					AND p.author_id = :userID)
+			  OR (p.visibility_type = 'FRIENDS'
+					AND p.author_id = :userID)
+			  OR (p.visibility_type = 'FRIENDS'
+					AND EXISTS (
+			        	SELECT 1
+			        	FROM connection c
+			        	WHERE c.connection_status = 'Accepted'
+			          	AND (
+			                (c.requester_id = p.author_id AND c.receiver_id = :userID)
+			             OR (c.receiver_id = p.author_id AND c.requester_id = :userID)
+			          	)
+			    )
+			   );
+			   """, nativeQuery = true)
 	List<Post> findAllPostOfUser(@Param("userID") UUID userID);
-	
-	@Query(
-			  value = """
-			    SELECT p.*
-			    FROM post p
-			    WHERE
-			      p.visibility_type = 'PUBLIC'
-			    """,
-			  nativeQuery = true
-			)
+
+	@Query(value = """
+			SELECT p.*
+			FROM post p
+			WHERE
+			  p.visibility_type = 'PUBLIC'
+			""", nativeQuery = true)
 	List<Post> findByVisibilityPublic();
 
 }
