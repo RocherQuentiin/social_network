cd8537daa4fdc3d7b1e373a1340f2354e7bad70f
Quentin <prorocher.quentin@gmail.com>
2026-01-13 23:26:53 +0100

    feat(service): add project service interfaces
    - Add IProjectService with CRUD operations
      * createProject, updateProject, deleteProject
      * getProjectById, getProjectsByCreator
      * getPublicProjects, getProjectsVisibleToUser
    - Add IProjectMemberService for member operations
      * addMemberToProject, removeMemberFromProject
      * getProjectMembers, leaveProject, deleteMember
      * getUserRoleInProject, isMemberOfProject

---
+++ socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/interfaces/service/IProjectService.java
@@ -0,0 +1,47 @@
+package com.socialnetwork.socialnetwork.business.interfaces.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+
+import com.socialnetwork.socialnetwork.dto.ProjectDto;
+import com.socialnetwork.socialnetwork.entity.Project;
+
+public interface IProjectService {
+
+    /**
+     * Create a new project
+     */
+    ResponseEntity<Project> createProject(ProjectDto projectDto, UUID creatorId);
+
+    /**
+     * Update an existing project
+     */
+    ResponseEntity<Project> updateProject(UUID projectId, ProjectDto projectDto, UUID userId);
+
+    /**
+     * Get a project by ID
+     */
+    ResponseEntity<Project> getProjectById(UUID projectId);
+
+    /**
+     * Get all projects created by a user
+     */
+    ResponseEntity<List<Project>> getProjectsByCreator(UUID userId);
+
+    /**
+     * Get all public projects
+     */
+    ResponseEntity<List<Project>> getPublicProjects();
+
+    /**
+     * Get projects visible to a user
+     */
+    ResponseEntity<List<Project>> getProjectsVisibleToUser(UUID userId);
+
+    /**
+     * Delete a project
+     */
+    ResponseEntity<Void> deleteProject(UUID projectId, UUID userId);
+}
