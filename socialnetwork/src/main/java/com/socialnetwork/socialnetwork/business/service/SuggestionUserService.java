9dcc2253e4be70b5135a81347ef12ad1905d4ad1
akash47 <65364908+Akashos23@users.noreply.github.com>
2026-01-14 18:33:08 +0100

    fin algo

--- socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/service/SuggestionUserService.java
+++ socialnetwork/src/main/java/com/socialnetwork/socialnetwork/business/service/SuggestionUserService.java
@@ -2,7 +2,9 @@ package com.socialnetwork.socialnetwork.business.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,35 @@ public class SuggestionUserService implements ISuggestionUserService{
 		this.connectionService = connectionService;
 	}
 	
+	public int findAllMatchBeetweenTwoList(List<Connection> userConnections, User user,  List<Connection> userConnectionsOfSuggestionUser, User userToSuggest) {
+		List<User> users = new ArrayList<>();
+		List<User> userSuggested = new ArrayList<>();
+		for(int i = 0; i < userConnections.size(); i++) {
+			if(!userConnections.get(i).getReceiver().equals(user)) {
+				users.add(userConnections.get(i).getReceiver());
+			}
+			
+			if(!userConnections.get(i).getRequester().equals(user)) {
+				users.add(userConnections.get(i).getRequester());
+			}
+		}
+		
+		for(int i = 0; i < userConnectionsOfSuggestionUser.size(); i++) {
+			if(!userConnectionsOfSuggestionUser.get(i).getReceiver().equals(userToSuggest)) {
+				userSuggested.add(userConnectionsOfSuggestionUser.get(i).getReceiver());
+			}
+			
+			if(!userConnectionsOfSuggestionUser.get(i).getRequester().equals(userToSuggest)) {
+				userSuggested.add(userConnectionsOfSuggestionUser.get(i).getRequester());
+			}
+		}
+		
+		List<User> common = new ArrayList<>(users);
+        common.retainAll(userSuggested);
+		
+		return common.size();
+	}
+	
 	@Override
 	public HashMap<String, String> getSuggestionUser(List<User> listUsers, User user){
 		HashMap<String, String> map = new HashMap<String, String>();
@@ -30,25 +61,45 @@ public class SuggestionUserService implements ISuggestionUserService{
 			boolean match = userConnections.stream().anyMatch(x -> x.getReceiver().equals(listUsers.get(j)) || x.getRequester().equals(listUsers.get(j)));
 			if(!match) {
 				List<Connection> userConnectionsOfSuggestionUser = this.connectionService.findAllAcceptedRequestByUserID(listUsers.get(i).getId());
-				List<Connection> common = new ArrayList<Connection>(userConnections);
-		        common.retainAll(userConnectionsOfSuggestionUser);
-				
-				if(common.size() > 0) {
-					suggestion += "Vous avez " + common.size() + " amis en commun";
+				int cptCommonFriends = findAllMatchBeetweenTwoList(userConnections, user, userConnectionsOfSuggestionUser, listUsers.get(i));
+				if(cptCommonFriends > 0) {
+					suggestion += "\n Vous avez " + cptCommonFriends + " amis en commun";
 				}
 		        
 				if(listUsers.get(i).getProfile().getIsepSpecialization() != null && user.getProfile().getIsepSpecialization() != null) {
 					if(listUsers.get(i).getProfile().getIsepSpecialization().equals(user.getProfile().getIsepSpecialization())) {
 						String specialization = user.getProfile().getIsepSpecialization().toString().replace('_', ' ');
-						suggestion += "Vous faites partie de la même spécialisation : " + specialization;
+						suggestion += "\n Vous faites partie de la même spécialisation : " + specialization;
 					}
 				}
 				
 				if(listUsers.get(i).getProfile().getPromoYear() != null && user.getProfile().getPromoYear() != null) {
 					if(listUsers.get(i).getProfile().getPromoYear().equals(user.getProfile().getPromoYear())) {
-						suggestion += "Vous faites partie de la même promo : " + user.getProfile().getPromoYear();
+						suggestion += "\n Vous faites partie de la même promo : " + user.getProfile().getPromoYear();
 					}
 				}
+				
+				if(user.getProfile().getInterests() != null && listUsers.get(i).getProfile().getInterests() != null) {
+					Set<Object> commonValues = new HashSet<>(user.getProfile().getInterests().values());
+			        commonValues.retainAll(listUsers.get(i).getProfile().getInterests().values());
+			        
+			        if(commonValues.size() > 0) {
+			        	suggestion += "\n Vous avez " + commonValues.size() + " loisirs en commun";
+			        }
+				}
+				
+				if(user.getProfile().getCompetencies() != null && listUsers.get(i).getProfile().getCompetencies() != null) {
+					Set<Object> commonValues = new HashSet<>(user.getProfile().getCompetencies().values());
+			        commonValues.retainAll(listUsers.get(i).getProfile().getCompetencies().values());
+			        
+			        if(commonValues.size() > 0) {
+			        	suggestion += "\n Vous avez " + commonValues.size() + " compétences en commun";
+			        }
+				}
+				
+				
+		        
+		        
 				
 				if(!suggestion.equals("")) {
 					map.put(listUsers.get(i).getId() + " " + listUsers.get(i).getLastName() + " " + listUsers.get(i).getFirstName(), suggestion);
